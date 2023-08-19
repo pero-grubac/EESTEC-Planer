7b60596a5b6c0692c9c80d910dfc00a20ec9324b
Sofija Rodić <74682145+Sofija1234@users.noreply.github.com>
2023-08-19 11:08:34 +0200

    Dodan AdminList za pristup bazi

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Login } from './Login';
 import { Main } from './Main';
 import { useState } from 'react';
+import AdminList from './AdminList';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className="App">
+      <AdminList></AdminList>
       
       {
         currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Main onFormSwitch={toggleForm}/>
