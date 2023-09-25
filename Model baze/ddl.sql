4127c3d0661d42717cf57bc23c3677a2cf44502e
Pero <perogrubac99@gmail.com>
2023-09-25 14:34:12 +0200

    Kreirana procedura

--- Model baze/ddl.sql
+++ Model baze/ddl.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `eestecplaner` /*!40100 DEFAULT CHARACTER SET utf
 USE `eestecplaner`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
--- Host: localhost    Database: eestecplaner
+-- Host: 127.0.0.1    Database: eestecplaner
 -- ------------------------------------------------------
 -- Server version	8.0.32
 
@@ -448,6 +448,44 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `DeleteUserAndAssociatedData` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`korisnik`@`localhost` PROCEDURE `DeleteUserAndAssociatedData`(IN UserId INT)
+BEGIN
+    DELETE FROM korisnik_radi_zadatak
+    WHERE Korisnik_IdKorisnika = UserId;
+
+    DELETE FROM zadatak
+    WHERE IdAutora = UserId;
+
+    DELETE FROM korisnik_pripada_timu
+    WHERE Korisnik_IdKorisnika = UserId;
+
+    UPDATE tim
+    SET IdKoordinator = NULL
+    WHERE IdKoordinator = UserId;
+
+    DELETE FROM koordinator WHERE IdKoordinator = UserId;
+
+    DELETE FROM clanodbora WHERE IdClana = UserId;
+
+    DELETE FROM superuser WHERE IdSuperuser = UserId;
+    
+    DELETE FROM korisnik WHERE IdKorisnika = UserId;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -458,4 +496,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-24 18:24:23
+-- Dump completed on 2023-09-25 14:33:50
